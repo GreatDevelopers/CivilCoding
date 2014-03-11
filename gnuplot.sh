a1d6525415236602db057ce578738582e23026d1
Piyush <achyutapiyush@gmail.com>
2014-03-11 10:58:15 +0530

    Using Jquery Validator

--- gnuplot.sh
+++ gnuplot.sh
@@ -4,7 +4,8 @@ set output "BM.png"
 set xlabel "x axis" tc rgb "black"
 set ylabel "y axis" tc rgb "red"
 set border linecolor rgbcolor "grey"
-set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 1.5   # --- blue
+set style line 1 lc rgb '#8b1a0e' pt 1 ps 1 lt 1 lw 2 # --- red
+set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2 # --- green
 plot "cantbm.out" with linespoints lt rgb "green"
 
 
@@ -14,5 +15,6 @@ set output "SF.png"
 set xlabel "x axis" tc rgb "black"
 set ylabel "y axis" tc rgb "red"
 set border linecolor rgbcolor "grey"
-set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 1.5   # --- blue
+set style line 1 lc rgb '#8b1a0e' pt 1 ps 1 lt 1 lw 2 # --- red
+set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2 # --- green
 plot "cantsf.out" with linespoints lt rgb "green"
